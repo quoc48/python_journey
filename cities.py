377ce589558ca8abefa53dc473a723d441559216
QuocP <53965851+quoc48@users.noreply.github.com>
2024-06-23 14:46:43 +0700

    practiced

---
+++ cities.py
@@ -0,0 +1,6 @@
+def describe_city(city_name, country = 'United of America'):
+    print(f"{city_name.title()} is in {country.title()}")
+
+describe_city('new york')
+describe_city('los angeles')
+describe_city('london','england')
