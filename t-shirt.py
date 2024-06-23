62619fe547e2b76f39c0903ff132949d66d29c86
QuocP <53965851+quoc48@users.noreply.github.com>
2024-06-23 14:40:45 +0700

    practiced

---
+++ t-shirt.py
@@ -0,0 +1,6 @@
+def make_shirt(size, message = 'This is my favorite style!'):
+    print(f"The size of the shirt is {size} with the message '{message}'")
+
+make_shirt('medium', 'Let do it!') # Using positional arguments
+make_shirt('large') # Using keyword arguments
+
