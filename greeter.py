03bf093eeed1c29fe4d9c2c10c24abec061646bc
QuocP <53965851+quoc48@users.noreply.github.com>
2024-06-23 15:19:03 +0700

    return function with a while loop

--- greeter.py
+++ greeter.py
@@ -1,5 +1,14 @@
-def greet_user(name):
-    """Display a simple greeting."""
-    print("Hello!", name)
+def get_formatted_name(first_name, last_name):
+    """Return a full name, neatly formatted."""
+    full_name = f"{first_name} {last_name}"
+    return full_name.title()
 
-greet_user("Quoc Phan")
+# This is an infinite loop
+while True:
+    print("\nPlease tell me your name.")
+    f_name = input("First name: ")
+    l_name = input("Last name: ")
+
+    formatted_name = get_formatted_name(f_name, l_name)
+    print(f"\nHello, {formatted_name}!")
+
