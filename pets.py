2a8c4121c434a7df6fa5c61c4d76673601928f34
QuocP <53965851+quoc48@users.noreply.github.com>
2024-06-23 14:33:52 +0700

    practiced

---
+++ pets.py
@@ -0,0 +1,9 @@
+def describe_pet(pet_name, animal_type = 'dog'):
+    """Display information about a pet"""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+describe_pet('harry', 'hamster')
+describe_pet('whillie')
+describe_pet(animal_type= 'fish', pet_name= 'terry')
+describe_pet()
