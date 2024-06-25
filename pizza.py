f8310dc35e7a1ea22a3842199656d965e4d870bb
QuocP <53965851+quoc48@users.noreply.github.com>
2024-06-25 14:10:18 +0700

    Turn into a module

--- pizza.py
+++ pizza.py
@@ -3,7 +3,3 @@ def make_pizza(size, *toppings):
     print(f"\nMaking a {size}-inch pizza with the following topping:")
     for topping in toppings:
         print(f"- {topping}")
-
-
-make_pizza(16,'pepperoni')
-make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
