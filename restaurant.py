fc05d9f17acf4771ce93e80ba4ce37fbfc1e5152
QuocP <53965851+quoc48@users.noreply.github.com>
2024-06-25 19:54:59 +0700

    Adding more methods

--- restaurant.py
+++ restaurant.py
@@ -3,21 +3,35 @@ class Restaurant:
     def __init__(self, name, type):
         self.name = name
         self.type = type
+        self.number_served = 0
 
 
     def describe_restaurant(self):
-        print(f"{self.name}'s restaurant type is {self.type}.")
+        print(f"\n--- Restaurant information ---")
+        print(f"- Restaurant name: {self.name}")
+        print(f"- Restaurant type: {self.type}")
 
 
     def open_restaurant(self):
         print(f"{self.name} restaurant is open!")
 
-first_restaurant = Restaurant('Little Cam', 'Coffee and Lunch')
-second_restaurant = Restaurant('Topping Beef', 'Beef Steak')
-third_restaurant = Restaurant('Domino','Pizza and Hambugar')
-first_restaurant.describe_restaurant()
-second_restaurant.describe_restaurant()
-third_restaurant.describe_restaurant()
+    def show_served(self):
+        print(f"- Number of served customer: {self.number_served}")
+
+    def set_number_served(self, number_served):
+        self.number_served = number_served
+
+    def increase_number_served(self, number):
+        self.number_served += number
+
+my_restaurant = Restaurant('Little Cam', 'Coffee and Lunch')
+
+my_restaurant.describe_restaurant()
+my_restaurant.set_number_served(32)
+my_restaurant.show_served()
+
+my_restaurant.increase_number_served(2)
+my_restaurant.show_served()
 
 
 
