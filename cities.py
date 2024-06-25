5c2d10d2c9383f76c9448bb137afb35e6e99c2b9
QuocP <53965851+quoc48@users.noreply.github.com>
2024-06-25 14:23:04 +0700

    turn into a module

--- cities.py
+++ cities.py
@@ -1,6 +1,9 @@
 def describe_city(city_name, country = 'United of America'):
+    """
+    Print out a city belong a country.
+    :param city_name:
+    :param country:
+    :return: print out a sentence
+    """
     print(f"{city_name.title()} is in {country.title()}")
 
-describe_city('new york')
-describe_city('los angeles')
-describe_city('london','england')
