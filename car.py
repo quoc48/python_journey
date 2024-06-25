7ef244047030423efe8ebb86db0488033a1c07e7
QuocP <53965851+quoc48@users.noreply.github.com>
2024-06-25 19:41:04 +0700

    setting a default value for an attribute in classes

--- car.py
+++ car.py
@@ -16,6 +16,26 @@ class Car:
         """Print a statement showing a car's mileage."""
         print(f"This car has {self.odometer_reading} mile on it.")
 
+    def update_odometer(self, mileage):
+        """
+        Set the odometer reading to the given value.
+        Reject the change if it attempt to roll the odometer back.
+        """
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer.")
+
+    def increment_odometer(self, miles):
+        """Add the given amount to the odometer reading."""
+        self.odometer_reading += miles
+
+
 my_new_car = Car('audi', 'a4', '2024')
 print(my_new_car.get_descriptive_name())
+
+my_new_car.update_odometer(25_500)
+my_new_car.read_odometer()
+
+my_new_car.increment_odometer(100)
 my_new_car.read_odometer()
