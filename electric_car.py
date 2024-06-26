f9fd61d0aded7c90a38c6e9801afba4890c02278
QuocP <53965851+quoc48@users.noreply.github.com>
2024-06-26 11:44:47 +0700

    override methods from the parent class

--- electric_car.py
+++ electric_car.py
@@ -27,14 +27,31 @@ class Car:
         """Add the given amount to the odometer reading."""
         self.odometer_reading += miles
 
+    def fill_gas_tank(self):
+        """Fill gas for car"""
+        print("This car's gas has been fill full.")
+
 class ElectricCar(Car):
     """Represent aspects of car, specific to electric vehicles."""
 
     def __init__(self, make, model, year):
-        """Initialize attributes of the parent class."""
+        """
+        Initialize attributes of the parent class.
+        Then initialize attributes specific to an electric car.
+        """
         super().__init__(make, model, year)
+        self.battery_size = 40
+
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print(f"This car has a {self.battery_size}--kWh battery.")
+
+    def fill_gas_tank(self):
+        """Electric cars don't have gas tank."""
+        print("This car doesn't have a gas tank.")
 
 
 my_leaf = ElectricCar('nissan', 'leaf', 2024)
 print(my_leaf.get_descriptive_name())
-
+my_leaf.describe_battery()
+my_leaf.fill_gas_tank()
