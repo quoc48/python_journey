8177bf461e23ac68fe88b8598ae30b795f40cbea
QuocP <53965851+quoc48@users.noreply.github.com>
2024-06-26 12:19:58 +0700

    instances as attributes

--- electric_car.py
+++ electric_car.py
@@ -31,6 +31,26 @@ class Car:
         """Fill gas for car"""
         print("This car's gas has been fill full.")
 
+class Battery:
+    """A simple attempt to model a battery for an electric car."""
+
+    def __init__(self, battery_size=40):
+        """Initialize the battery's attribute."""
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print(f"This car has a {self.battery_size}--kWh battery.")
+
+    def get_range(self):
+        """Print a statement about the range this battery provides."""
+        if self.battery_size == 40:
+            range = 150
+        elif self.battery_size == 65:
+            range = 225
+
+        print(f"This car can go about {range} miles on a full charge.")
+
 class ElectricCar(Car):
     """Represent aspects of car, specific to electric vehicles."""
 
@@ -40,11 +60,7 @@ class ElectricCar(Car):
         Then initialize attributes specific to an electric car.
         """
         super().__init__(make, model, year)
-        self.battery_size = 40
-
-    def describe_battery(self):
-        """Print a statement describing the battery size."""
-        print(f"This car has a {self.battery_size}--kWh battery.")
+        self.battery = Battery()
 
     def fill_gas_tank(self):
         """Electric cars don't have gas tank."""
@@ -53,5 +69,5 @@ class ElectricCar(Car):
 
 my_leaf = ElectricCar('nissan', 'leaf', 2024)
 print(my_leaf.get_descriptive_name())
-my_leaf.describe_battery()
-my_leaf.fill_gas_tank()
+my_leaf.battery.describe_battery()
+my_leaf.battery.get_range()
