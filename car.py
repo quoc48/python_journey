68ddd0612234e94f636bb16abd8b712e8c6bf3c7
QuocP <53965851+quoc48@users.noreply.github.com>
2024-06-27 11:04:00 +0700

    remove the execution part of the class out

--- car.py
+++ car.py
@@ -30,12 +30,3 @@ class Car:
         """Add the given amount to the odometer reading."""
         self.odometer_reading += miles
 
-
-my_new_car = Car('audi', 'a4', '2024')
-print(my_new_car.get_descriptive_name())
-
-# my_new_car.update_odometer(25_500)
-my_new_car.read_odometer()
-
-my_new_car.increment_odometer(100)
-my_new_car.read_odometer()
