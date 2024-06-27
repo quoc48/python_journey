4d51fc02207c65961a01eae08325bf70569cd8e1
QuocP <53965851+quoc48@users.noreply.github.com>
2024-06-27 12:00:59 +0700

    update style

--- admin.py
+++ admin.py
@@ -4,4 +4,5 @@ from privileges import Privileges
 class Admin(User):
     def __init__(self, first_name, last_name, user_age):
         super().__init__(first_name, last_name, user_age)
-        self.privileges = Privileges('can add post', 'can delete post', 'can ban user')
+        self.privileges = Privileges('can add post', 'can delete post',
+                                     'can ban user')
