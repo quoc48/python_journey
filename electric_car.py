bc769bc7fb86a33feeed0faa3ca45c23e8e12a4f
QuocP <53965851+quoc48@users.noreply.github.com>
2024-06-27 11:36:34 +0700

    move out Car class

--- electric_car.py
+++ electric_car.py
@@ -1,36 +1,4 @@
-class Car:
-    """A simple attempt to represent a car."""
-
-    def __init__(self, make, model, year):
-        self.make = make
-        self.model = model
-        self.year = year
-        self.odometer_reading = 0
-
-    def get_descriptive_name(self):
-        """Return a neatly formatted descriptive name."""
-        long_name = f"{self.year} {self.make} {self.model}"
-        return long_name.title()
-
-    def read_odometer(self):
-        """Print a statement showing the car's mileage."""
-        print(f"This car has {self.read_odometer()} mile on it.")
-
-    def update_read_odometer(self, miliage):
-        """Set the odometer reading to the given value."""
-        if miliage >= self.odometer_reading:
-            self.odometer_reading = miliage
-        else:
-            print("You can't roll back an odometer!")
-
-    def increment_odometer(self, miles):
-        """Add the given amount to the odometer reading."""
-        self.odometer_reading += miles
-
-    def fill_gas_tank(self):
-        """Fill gas for car"""
-        print("This car's gas has been fill full.")
-
+from car import Car
 class Battery:
     """A simple attempt to model a battery for an electric car."""
 
