65a03892633d84b1a1aa635c997bd4e1d2cd259e
QuocP <53965851+quoc48@users.noreply.github.com>
2024-06-28 23:28:15 +0700

    is your birthday contained in Pi

--- files_and_exceptions/pi_birthday.py
+++ files_and_exceptions/pi_birthday.py
@@ -10,7 +10,7 @@ for line in lines:
 
 birthday = input("Enter your birthday, in the form mmddyy: ")
 if birthday in pi_string:
-    print("Your birthday appears in the first milion digit of pi!")
+    print("Your birthday appears in the first million digit of pi!")
 else:
-    print("Your birthday does not appear in the first milion digits of pi.")
+    print("Your birthday does not appear in the first million digits of pi.")
 
