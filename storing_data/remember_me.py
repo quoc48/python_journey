1117c1ffa78702ab1d529e6c216c242f32326ec1
QuocP <53965851+quoc48@users.noreply.github.com>
2024-06-30 14:05:38 +0700

    refactoring code and adding new usecase

--- storing_data/remember_me.py
+++ storing_data/remember_me.py
@@ -6,26 +6,32 @@ def get_stored_username(path):
     """Get stored username if available."""
     if path.exists() and path.stat().st_size > 0:
         contents = path.read_text()
-        username = json.loads(contents)
-        return username
+        user_profile = json.loads(contents)
+        return user_profile
     else:
         return None
 
 def get_new_username(path):
     """Prompt for a new username."""
-    username = input("What's your name? ")
-    contents = json.dumps(username)
+    user_profile = {}
+    user_profile['username'] = input("What's your name? ")
+    user_profile['age'] = input("How old are you? ")
+    user_profile['major'] = input("What is your major? ")
+    contents = json.dumps(user_profile)
     path.write_text(contents)
-    return username
+    return user_profile
 
 def greet_user():
     """Greet the user by name."""
     path = Path('username.json')
-    username = get_stored_username(path)
-    if username:
-        print(f"Welcome back, {username}!")
+    user_profile = get_stored_username(path)
+    confirm = input(f"You are {user_profile['username']}? (y/n)")
+    if user_profile and confirm.lower() == 'y':
+        print("\nWelcome back!")
+        for key, value in user_profile.items():
+            print(f"- {key}: {value}")
     else:
-        username = get_new_username(path)
-        print(f"We'll remember you when you come back, {username}.")
+        user_profile = get_new_username(path)
+        print(f"We'll remember you when you come back, {user_profile['username']}.")
 
 greet_user()
