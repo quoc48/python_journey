daf64513140115949703687e5169220bb98fbc07
QuocP <53965851+quoc48@users.noreply.github.com>
2024-07-07 11:18:25 +0700

    making the fleet move

--- alien_invasion/alien.py
+++ alien_invasion/alien.py
@@ -27,6 +27,6 @@ class Alien(Sprite):
         return (self.rect.right >= screen_rect.right) or (self.rect.left <= 0)
 
     def update(self):
-        """Move the alien to the right."""
+        """Move the alien right or left."""
         self.x += self.settings.alien_speed * self.settings.fleet_direction
         self.rect.x = self.x
