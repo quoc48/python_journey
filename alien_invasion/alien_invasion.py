eda37e93c7fccb905675312e5a3c7eade4bb8463
QuocP <53965851+quoc48@users.noreply.github.com>
2024-07-09 21:26:32 +0700

    leveling up

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -95,6 +95,9 @@ class AlienInvasion:
         """Start a new game when the player clicks Play."""
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.game_active:
+            # Reset the game settings.
+            self.settings.initialize_dynamic_settings()
+
             # Reset the game statistics.
             self.stats.reset_stats()
             self.game_active = True
@@ -158,6 +161,7 @@ class AlienInvasion:
             # Detroy existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
 
     def _update_aliens(self):
         """Check if the fleet is at an edge, then update position."""
