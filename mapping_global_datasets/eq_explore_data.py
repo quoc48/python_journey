663a01c40827b696e1faed45a2cc98ff0580fb9f
QuocP <53965851+quoc48@users.noreply.github.com>
2024-07-21 22:05:48 +0700

    representing magnitudes

--- mapping_global_datasets/eq_explore_data.py
+++ mapping_global_datasets/eq_explore_data.py
@@ -4,7 +4,7 @@ import json
 import plotly.express as px
 
 # Read data as a string and convert to a Python object.
-path = Path('eq_data/eq_data_1_day_m1.geojson')
+path = Path('eq_data/eq_data_30_day_m1.geojson')
 contents = path.read_text()
 all_eq_data = json.loads(contents)
 
@@ -26,5 +26,5 @@ for eq_dict in all_eq_dicts:
     lats.append(lat)
 
 title = 'Global Earthquakes'
-fig = px.scatter_geo(lat=lats, lon=lons, title=title)
+fig = px.scatter_geo(lat=lats, lon=lons, size=mags, title=title)
 fig.show()
