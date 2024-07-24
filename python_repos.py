5ec24f302801a581492925281479db05c1add68f
QuocP <53965851+quoc48@users.noreply.github.com>
2024-07-24 22:03:03 +0700

    summary the top repositories

--- python_repos.py
+++ python_repos.py
@@ -19,16 +19,17 @@ repo_dicts = response_dict['items']
 print(f"Repositories returned: {len(repo_dicts)}")
 
 # Examine the first repository.
-repo_dict = repo_dicts[1]
+#repo_dict = repo_dicts[1]
 # print(f"\nKey: {len(repo_dict)}")
 #for key in sorted(repo_dict.keys()):
    # print(key)
 
-print("\nSelected information about first repository:")
-print(f"Name: {repo_dict['name']}")
-print(f"Owner: {repo_dict['owner']['login']}")
-print(f"Stars: {repo_dict['stargazers_count']}")
-print(f"Repository: {repo_dict['html_url']}")
-print(f"Created: {repo_dict['created_at']}")
-print(f"Updated: {repo_dict['updated_at']}")
-print(f"Description: {repo_dict['description']}")
+print("\nSelected information about each repository:")
+for repo_dict in repo_dicts:
+    print(f"\nName: {repo_dict['name']}")
+    print(f"Owner: {repo_dict['owner']['login']}")
+    print(f"Stars: {repo_dict['stargazers_count']}")
+    print(f"Repository: {repo_dict['html_url']}")
+    print(f"Created: {repo_dict['created_at']}")
+    print(f"Updated: {repo_dict['updated_at']}")
+    print(f"Description: {repo_dict['description']}")
