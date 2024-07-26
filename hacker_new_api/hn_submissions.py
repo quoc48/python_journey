32cb07bb5b583f634cc88d4ad7cbe304a1c7b48b
QuocP <53965851+quoc48@users.noreply.github.com>
2024-07-26 21:56:39 +0700

    add bar chart for active discussion

--- hacker_new_api/hn_submissions.py
+++ hacker_new_api/hn_submissions.py
@@ -1,6 +1,7 @@
 from operator import itemgetter
 
 import requests
+import plotly.express as px
 
 # Make an API call and check the response.
 url = f"https://hacker-news.firebaseio.com/v0/topstories.json"
@@ -10,12 +11,22 @@ print(f"Status code: {r.status_code}")
 # Process information about each submission.
 submission_ids = r.json()
 submission_dicts = []
-for submission_id in submission_ids[:5]:
+repo_links, comments = [], []
+for submission_id in submission_ids[:10]:
     # Make a new API call for each submission.
     url = f"https://hacker-news.firebaseio.com/v0/item/{submission_id}.json"
     r = requests.get(url)
     print(f"id: {submission_id}\tstatus: {r.status_code}")
     response_dict = r.json()
+    try:
+        repo_name = response_dict['title']
+        repo_url = response_dict['url']
+        repo_link = f"<a href='{repo_url}'>{repo_name}</a>"
+        repo_links.append(repo_link)
+    except:
+        continue
+
+    comments.append(response_dict['descendants'])
 
     # Build a dictionary for each article.
     submission_dict = {
@@ -24,6 +35,15 @@ for submission_id in submission_ids[:5]:
         'comments': response_dict['descendants'],
     }
     submission_dicts.append(submission_dict)
+
+# Make visualization.
+title = "Most active discussion on Hacker News."
+label = {'x': 'Submission', 'y': 'Number of comment'}
+fig = px.bar(x=repo_links, y=comments, title=title, labels=label)
+
+fig.update_layout(title_font_size=28, xaxis_title_font_size=20,
+                  yaxis_title_font_size=20)
+fig.show()
 
 submission_dicts = sorted(submission_dicts, key=itemgetter('comments'),
                           reverse=True)
